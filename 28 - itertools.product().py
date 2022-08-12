862c99ea2248af6fc08e78a6d616236ce59a117c
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-12 07:43:33 +0700

    Create 28 - itertools.product().py

---
+++ 28 - itertools.product().py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+
+a = map(int, input().split())
+b = map(int, input().split())
+
+print(*product(a, b))
