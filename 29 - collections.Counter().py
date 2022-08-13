130a62b508fbcd9419fe3c2b82521c5a395ec768
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-13 22:41:16 +0700

    Create 29 - collections.Counter().py

---
+++ 29 - collections.Counter().py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+num_shoes = int(input())
+shoes = Counter(map(int, input().split()))
+num_customer = int(input())
+
+income = 0
+
+for i in range(num_customer):
+    size, price = map(int, input().split())
+    if shoes[size]:
+        income += price
+        shoes[size] -= 1
+print(income)
